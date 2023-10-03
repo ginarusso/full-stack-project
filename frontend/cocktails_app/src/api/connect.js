c7f56bfee763ed159dd4ca79887adbcb03b5c7d4
Gina Russo <gpr0496@gmail.com>
2023-10-02 23:31:56 -0400

    adding new URL to the frontend

--- frontend/cocktails_app/src/api/connect.js
+++ frontend/cocktails_app/src/api/connect.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiConn = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://backend-for-cocktails-app.onrender.com'
 });
 
 export default apiConn;
